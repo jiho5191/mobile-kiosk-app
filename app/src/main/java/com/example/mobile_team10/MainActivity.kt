526803d5e058f9ad5f27eab245c3980fd647e03f
seungje1217 <sjlim1217@naver.com>
2025-05-16 12:18:27 +0900

    주문내역

--- app/src/main/java/com/example/mobile_team10/MainActivity.kt
+++ app/src/main/java/com/example/mobile_team10/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mobile_team10
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -45,6 +46,22 @@ class MainActivity : AppCompatActivity() {
         binding.cartButton.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
+        }
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_order_history -> {
+                val intent = Intent(this, OrderHistoryActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
